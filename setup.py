a62105733f12eb53e205013bb102da64d3a780bf
Andrea Cecchi <andrea.cecchi85@gmail.com>
2020-01-10 16:36:24 +0100

    Back to development: 1.0.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = '\n\n'.join(
 
 setup(
     name='cciaapd.userdata',
-    version='1.0.0',
+    version='1.0.1.dev0',
     description="Additional userdata for cciaapd",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
